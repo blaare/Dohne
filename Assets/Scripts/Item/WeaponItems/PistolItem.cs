b7202e804aa91aa8167c9424d1fd761bc358f43e
Timothy Wilson <blaare@protonmail.com>
2018-11-28 18:46:25 -0500

    renamed some of the items

--- Assets/Scripts/Item/WeaponItems/PistolItem.cs
+++ Assets/Scripts/Item/WeaponItems/PistolItem.cs
@@ -9,25 +9,37 @@ public class PistolItem : Item
 
     public override void Pickup(GameObject player)
     {
-        Destroy(gameObject);
         //Get the player's WeaponSelector
         WeaponSelector weaponSelector = player.GetComponent<WeaponSelector>();
 
         //Make sure that you don't add duplicate weapons to the player's weapons list
         foreach (GameObject weapon in weaponSelector.weapons)
         {
-            if(weapon.gameObject.name == "Pistol")
+            if (weapon.gameObject.name == "Pistol")
             {
-                weapon.GetComponent<Blaster>().IncreaseAmmo(quantity);
-                Debug.Log("Increased Ammo Count by " + quantity.ToString());
-                return;
+                if (weapon.GetComponent<Blaster>().IncreaseAmmo(quantity))
+                {
+                    Destroy(gameObject);
+                    Debug.Log("Increased Ammo Count by " + quantity.ToString());
+                    return;
+                }
+                else
+                {
+                    Debug.Log("Max Ammo Reached, Leaving object alone");
+                    return;
+                }
             }
         }
+        Destroy(gameObject);
+        Debug.Log("Picked up Rifle");
 
-        weaponSelector.weapons.Add(pistolPrefab);
+        //Make sure the transforms are parented, correctly.
+        GameObject weapons = player.transform.GetChild(2).gameObject;
+        var newWeapon = Instantiate(pistolPrefab, weapons.transform);
+        //newWeapon.transform.localPosition = new Vector3(0, 0, 0);
+        newWeapon.name = "Rifle";
+        newWeapon.SetActive(false);
+        weaponSelector.weapons.Add(newWeapon);
 
-
-
-        
     }
 }
