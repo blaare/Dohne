2be52aca6349c096f9813ca6ac67431d00796fc2
Timothy Wilson <blaare@protonmail.com>
2018-11-29 05:57:37 -0500

    added the MouseHider script and added health/armor ui

---
+++ Assets/Scripts/UI/MouseHider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (!Application.isEditor)
+        {
+            Cursor.visible = false;
+        }
+    }
+}
