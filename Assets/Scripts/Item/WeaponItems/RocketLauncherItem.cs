73b7140ed5fbe9f95d0a69f988d98135bffce903
Timothy Wilson <blaare@protonmail.com>
2018-11-28 20:47:06 -0500

    updated physics layers and added rocket launcher pickups

---
+++ Assets/Scripts/Item/WeaponItems/RocketLauncherItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncherItem : Item {
+
+    public GameObject rocketLauncherPrefab;
+
+    public override void Pickup(GameObject player)
+    {
+
+        //Get the player's WeaponSelector
+        WeaponSelector weaponSelector = player.GetComponent<WeaponSelector>();
+
+        //Make sure that you don't add duplicate weapons to the player's weapons list
+        foreach (GameObject weapon in weaponSelector.weapons)
+        {
+            if (weapon.gameObject.name == "RocketLauncher")
+            {
+                if (weapon.GetComponent<Blaster>().IncreaseAmmo(quantity))
+                {
+                    Destroy(gameObject);
+                    Debug.Log("Increased Ammo Count by " + quantity.ToString());
+                    return;
+                }
+                else
+                {
+                    Debug.Log("Max Ammo Reached, Leaving object alone");
+                    return;
+                }
+            }
+        }
+        Destroy(gameObject);
+        Debug.Log("Picked up RocketLauncher");
+
+        //Make sure the transforms are parented, correctly.
+        GameObject weapons = player.transform.GetChild(2).gameObject;
+        var newWeapon = Instantiate(rocketLauncherPrefab, weapons.transform);
+        //newWeapon.transform.localPosition = new Vector3(0, 0, 0);
+        newWeapon.name = "RocketLauncher";
+        newWeapon.SetActive(false);
+        weaponSelector.weapons.Add(newWeapon);
+
+
+    }
+}
