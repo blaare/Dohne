098309072981402fef501a0a7707060cad317ba9
Nicholas Ricciuti <rishooty@gmail.com>
2018-12-01 13:27:25 -0500

    Refactored and improved AI

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -1,55 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Based on this tutorial: https://www.youtube.com/watch?v=ovEIluVrQjY&feature=youtu.be&t=451
 public class EnemyFollow : MonoBehaviour {
-
     public GameObject ThePlayer;
-    public float DistanceFromHeardTarget;
-    public float DistanceFromSeenTarget;
-    public float HearingRange = 10;
+    public bool chase = false;
+    public float TargetDistance;
+    public float AllowedRange = 10;
     public GameObject TheEnemy;
-    public float EnemySpeed;
+    public float EnemySpeed = 0.05f;
     public int AttackTrigger;
-    public RaycastHit Hearing;
-    RaycastHit Sight;
-    public float SightRange = 20;
+    public RaycastHit Shot;
+    int layerMask = 1 << 14;
 
     void Update() {
-        //see stuff in layer 14
-        int layerMask = 1 << 14;
+        if (chase) {
+            EnemySpeed = 0.05f;
+            transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
+        }
+        else {
+            EnemySpeed = 0;
+            //TheEnemy.GetComponent<Animation>().Play("Idle");
+        }
 
-        // If player is within "hearing range" or within line of sight.
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hearing, HearingRange, layerMask) ||
-            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Sight, SightRange, layerMask)) {
-
-            DistanceFromHeardTarget = Hearing.distance;
-            DistanceFromSeenTarget = Sight.distance;
-
-            if (DistanceFromHeardTarget < HearingRange || DistanceFromSeenTarget < SightRange) {
-                Debug.Log("CUBE THINKS YOU HAVE TOO MANY DIMENSIONS.");
-                transform.LookAt(ThePlayer.transform);
-                EnemySpeed = 0.08f;
-                if (AttackTrigger == 0) {
-                    //TheEnemy.GetComponent<Animation>().Play("Walking");
-                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
-                }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot, AllowedRange, layerMask) ||
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out Shot, AllowedRange, layerMask) ||
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out Shot, AllowedRange, layerMask)) {
+            transform.LookAt(ThePlayer.transform);
+            if (AttackTrigger == 0) {
+                //TheEnemy.GetComponent<Animation>().Play("Walking");
+                chase = true;
             }
         }
 
-        if(AttackTrigger == 1) {
-            EnemySpeed = 0;
+        if (AttackTrigger == 1) {
             //TheEnemy.GetComponent<Animation>().Play("Attacking");
             Debug.Log("CUBE HAS ENGAGED IN COMBAT.");
         }
     }
-
     void OnTriggerEnter() {
+        chase = false;
         AttackTrigger = 1;
     }
-
     void OnTriggerExit() {
+        chase = true;
         AttackTrigger = 0;
-        EnemySpeed = 0.02f;
     }
 }
