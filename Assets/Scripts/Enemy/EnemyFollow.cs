537ff0807eaa0ab3738780d584a6a6dcb93b3a20
Nicholas Ricciuti <rishooty@gmail.com>
2018-12-03 18:54:13 -0500

    fixed enemy raycast issues, massively simplified AI

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -6,7 +6,8 @@ public class EnemyFollow : MonoBehaviour {
     public GameObject ThePlayer;
     public bool chase = false;
     public float TargetDistance;
-    public float SightRange = 10;
+    public float SightRange = 20;
+    public float AttackRange = 10;
     public GameObject TheEnemy;
     public float EnemySpeed = 0.05f;
     public int MeleeAttackTrigger;
@@ -14,42 +15,23 @@ public class EnemyFollow : MonoBehaviour {
     int layerMask = 1 << 14;
 
     void Update() {
-        if (chase) {
-            EnemySpeed = 0.05f;
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Run(loop)");
-            transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
-        }
-        else {
-            EnemySpeed = 0;
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Idle");
-        }
-
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot, SightRange, layerMask) ||
             Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out Shot, SightRange, layerMask) ||
             Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out Shot, SightRange, layerMask)) {
             transform.LookAt(ThePlayer.transform);
-            if (MeleeAttackTrigger == 0) {
-                TheEnemy.GetComponent<Animation>().Play("Robo1 Run start");
-                chase = true;
+            chase = true;
+            if (Shot.distance < AttackRange) {
+                TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
+                Debug.Log("CUBE IS FIRING AT YOU.");
             }
-            // if (Shot.distance < AttackRange)
-                 //TheEnemy.GetComponent<Animation>().Play("Attacking");
-                 //Debug.Log("CUBE IS FIRING AT YOU.");
         }
-
-        if (MeleeAttackTrigger == 1) {
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Run end");
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Attack start");
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
+        else {
+            if (chase) {
+                transform.LookAt(ThePlayer.transform);
+                TheEnemy.GetComponent<Animation>().Play("Robo1 Run(loop)");
+                Debug.Log("CUBE IS CHASING YOU");
+                transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
+            }
         }
-    }
-    void OnTriggerEnter() {
-        chase = false;
-        MeleeAttackTrigger = 1;
-    }
-    void OnTriggerExit() {
-        TheEnemy.GetComponent<Animation>().Play("Robo1 Attack end");
-        chase = true;
-        MeleeAttackTrigger = 0;
     }
 }
