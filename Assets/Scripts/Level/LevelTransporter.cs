3fef09bc20c9d3d1280a459c08c77391d2632cb3
Timothy Wilson <blaare@protonmail.com>
2018-12-03 11:40:31 -0500

    updated level 1

---
+++ Assets/Scripts/Level/LevelTransporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransporter : MonoBehaviour {
+
+
+    public int sceneInBuildToLoad;
+	void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneInBuildToLoad);
+        }
+    }
+}
