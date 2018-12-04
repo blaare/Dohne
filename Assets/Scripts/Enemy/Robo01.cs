6d726983ca5689a66560a735b30fa02b84ee16e0
Nicholas Ricciuti <rishooty@gmail.com>
2018-12-03 22:14:42 -0500

    added ability to attack the player

--- Assets/Scripts/Enemy/Robo01.cs
+++ Assets/Scripts/Enemy/Robo01.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 // Based on this tutorial: https://www.youtube.com/watch?v=ovEIluVrQjY&feature=youtu.be&t=451
-public class EnemyFollow : MonoBehaviour {
+public class Robo01 : MonoBehaviour {
     public GameObject ThePlayer;
     public bool chase = false;
-    public float TargetDistance;
     public float SightRange = 20;
     public GameObject TheEnemy;
     public float EnemySpeed = 0.05f;
     public RaycastHit Shot;
+    public int health;
+    public int damagePerHit;
+    public float attackSpeed;
     int layerMask = 1 << 14;
 
     void Update() {
@@ -18,16 +20,30 @@ public class EnemyFollow : MonoBehaviour {
             Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out Shot, SightRange, layerMask)) {
             transform.LookAt(ThePlayer.transform);
             chase = true;
-            TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
-            Debug.Log("CUBE IS FIRING AT YOU.");
+            InvokeRepeating("Attack", 0f, attackSpeed);
         }
         else {
             if (chase) {
                 transform.LookAt(ThePlayer.transform);
                 TheEnemy.GetComponent<Animation>().Play("Robo1 Run(loop)");
-                Debug.Log("CUBE IS CHASING YOU");
                 transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
             }
         }
+    }
+
+    public void TakeHit(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    public void Attack() {
+        TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
+        ThePlayer.GetComponent<PlayerDefenseManager>().TakeDamage(damagePerHit);
+    }
+
+    public void Die() {
+        Destroy(gameObject);
     }
 }
