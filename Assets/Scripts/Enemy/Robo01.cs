d23abf27c6afd954e8e0661457b24224291dd2af
Timothy Wilson <blaare@protonmail.com>
2018-12-04 15:09:33 -0500

    added decent weapon icons

--- Assets/Scripts/Enemy/Robo01.cs
+++ Assets/Scripts/Enemy/Robo01.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 // Based on this tutorial: https://www.youtube.com/watch?v=ovEIluVrQjY&feature=youtu.be&t=451
-public class Robo01 : MonoBehaviour {
+public class Robo01 : Enemy {
     public GameObject ThePlayer;
     public bool chase = false;
     public float SightRange = 20;
     public GameObject TheEnemy;
     public float EnemySpeed = 0.05f;
     public RaycastHit Shot;
-    public int health;
-    public int damagePerHit;
     public float attackSpeed;
     int layerMask = 1 << 14;
 
@@ -31,19 +29,21 @@ public class Robo01 : MonoBehaviour {
         }
     }
 
-    public void TakeHit(int damage) {
+    public override void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public override void TakeHit(int damage) {
         health -= damage;
         if (health <= 0) {
             Die();
         }
     }
 
-    public void Attack() {
+    public override void Attack() {
         TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
         ThePlayer.GetComponent<PlayerDefenseManager>().TakeDamage(damagePerHit);
     }
 
-    public void Die() {
-        Destroy(gameObject);
-    }
 }
