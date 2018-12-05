468c82b5adff2bf6c7ba1879e513f986a3af1461
Nicholas Ricciuti <rishooty@gmail.com>
2018-12-04 20:13:10 -0500

    Fixed robot sight, they now see at an exact angle and distance. It's completely separate from firing range.

--- Assets/Scripts/Enemy/Robo01.cs
+++ Assets/Scripts/Enemy/Robo01.cs
@@ -5,28 +5,31 @@ using UnityEngine;
 public class Robo01 : Enemy {
     public GameObject ThePlayer;
     public bool chase = false;
-    public float SightRange = 20;
+    public float attackRange = 20;
+    public float sightRange = 30;
     public GameObject TheEnemy;
     public float EnemySpeed = 0.05f;
     public RaycastHit Shot;
-    public float attackSpeed;
+    public float attackSpeed = 3;
     int layerMask = 1 << 14;
 
     void Update() {
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot, SightRange, layerMask) ||
-            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out Shot, SightRange, layerMask) ||
-            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out Shot, SightRange, layerMask)) {
-            transform.LookAt(ThePlayer.transform);
+        Vector3 targetDir = ThePlayer.transform.position - transform.position;
+        float distance = Vector3.Distance(targetDir, transform.forward);
+        float angle = Vector3.Angle(targetDir, transform.forward);
+        if (angle < 30 && distance < sightRange && !chase) {
             chase = true;
-            InvokeRepeating("Attack", 0f, attackSpeed);
         }
-        else {
-            if (chase) {
+        if (chase) {
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot, attackRange, layerMask)){
+                Attack();
+            }
+            else {
                 transform.LookAt(ThePlayer.transform);
                 TheEnemy.GetComponent<Animation>().Play("Robo1 Run(loop)");
                 transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
             }
-        }
+        }  
     }
 
     public override void Die()
@@ -45,5 +48,4 @@ public class Robo01 : Enemy {
         TheEnemy.GetComponent<Animation>().Play("Robo1 Attack(loop)");
         ThePlayer.GetComponent<PlayerDefenseManager>().TakeDamage(damagePerHit);
     }
-
 }
