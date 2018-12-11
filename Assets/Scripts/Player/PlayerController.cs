3bbbde0fc11ac75d25d832e03287c8ca35917e5a
Timothy Wilson <blaare@protonmail.com>
2018-12-11 13:55:40 -0500

    added level2 placeholder

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour {
     void Start()
     {
         playerDefenseManager = GetComponent<PlayerDefenseManager>();
-        
+        DontDestroyOnLoad(this);
     }
 
 
